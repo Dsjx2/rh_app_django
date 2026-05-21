8426d1b43441f96f5a6d844c2923a75a36585ddf
Daniel <dsjsubx2.4c@gmail.om>
2026-05-20 22:11:31 -0600

    config deploy

--- rh_django/settings.py
+++ rh_django/settings.py
@@ -28,9 +28,11 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-# ALLOWED_HOSTS = ['dsjx2.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
